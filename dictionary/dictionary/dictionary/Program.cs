681efaf07dc65c8f9a61491240663c4ca0138ebf
judosucks <judosucks6827@gmail.com>
2022-02-17 20:41:32 +0800

    dic

--- dictionary/dictionary/dictionary/Program.cs
+++ dictionary/dictionary/dictionary/Program.cs
@@ -6,7 +6,61 @@ namespace dictionary
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee[] employees =
+            {
+                new Employee("ceo","judo",42,200),
+                new Employee("Manager","joe",35,25),
+                new Employee("pm","mary",32,21),
+                new Employee("developer","rik",42,35),
+                new Employee("artist","rikrik",42,30)
+            };
+            Dictionary<int, string> myDic = new Dictionary<int, string>()
+            {
+                {1,"one" },
+                {2,"two" },
+                {3,"three" }
+            };
+
+            Dictionary<string, Employee> employeesDic = new Dictionary<string, Employee>();
+            foreach(Employee emp in employees)
+            {
+                employeesDic.Add(emp.Role, emp);
+            }
+            string key = "ceo";
+            if (employeesDic.ContainsKey(key))
+            {
+                Employee emp1 = employeesDic[key];
+                Console.WriteLine("Employee name {0},role{1},salary{2}", emp1.Name, emp1.Role, emp1.Salary);
+            }
+            else
+            {
+                Console.WriteLine("no employee found with this key {0}", key);
+            }
+            
+        }
+    }
+    class Employee
+    {
+        //few properties like role name age and rate
+        public string Role { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Rate { get; set; }
+        // yearly salary = rate/h * number of days * number of weeks* num of months
+        public float Salary
+        {
+            get
+            {
+                return Rate * 8 * 5 * 4 * 12;
+            }
+        }
+        // simple constructor
+        public Employee(string role,string name,int age,float rate)
+        {
+            this.Role = role;
+            this.Name = name;
+            this.Age = age;
+            this.Rate = rate;
         }
     }
 }
